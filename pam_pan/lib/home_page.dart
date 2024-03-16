171dcb6e25e4f21f92f225f3cfa4ce101c8171ca
KhalilKK <khairkhalilkhaled@gmail.com>
2024-03-16 14:12:25 +0400

    home page (incomplete - just an update)

--- pam_pan/lib/home_page.dart
+++ pam_pan/lib/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -22,6 +21,7 @@ class HomePage extends StatelessWidget {
         icon: Image.asset(
           'assets/images/Khalil2.jpeg',
           scale: 7,
+          // tbh didn't have an icon in mind so I put a picture of me for funsies
         ),
         label: "XP"),
     NavigationDestination(
@@ -34,21 +34,15 @@ class HomePage extends StatelessWidget {
     // design some, but they have to be transparent
     IconButton(
       icon: const Icon(Icons.menu),
-      onPressed: () {
-        // Handle leading icon tap
-      },
+      onPressed: () {},
     ),
     IconButton(
       icon: const Icon(Icons.search),
-      onPressed: () {
-        // Handle search action tap
-      },
+      onPressed: () {},
     ),
     IconButton(
       icon: const Icon(Icons.settings),
-      onPressed: () {
-        // Handle settings action tap
-      },
+      onPressed: () {},
     ),
   ];
 
@@ -58,40 +52,43 @@ class HomePage extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             title: const Text('My App'),
-            leading: _appBarButtonList[0],
+            // leading: _appBarButtonList[0],
             actions: [
               _appBarButtonList[1],
               _appBarButtonList[2],
             ],
             backgroundColor: const Color.fromARGB(255, 91, 115, 136),
           ),
-          body: Container(
-            padding: const EdgeInsets.all(8),
-            child: Center(
-              child: PageView(
-                children: <Widget>[
-                  // Pages containing different content
-                  Container(
-                    color: Colors.blue,
-                    child: const Center(
-                      child: Text('Page 1'),
+          body: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                padding: const EdgeInsets.all(8),
+                child: PageView(
+                  children: <Widget>[
+                    // Pages containing different content
+                    Container(
+                      color: Colors.blue,
+                      child: const Center(
+                        child: Text('Page 1'),
+                      ),
                     ),
-                  ),
-                  Container(
-                    color: Colors.green,
-                    child: const Center(
-                      child: Text('Page 2'),
+                    Container(
+                      color: Colors.green,
+                      child: const Center(
+                        child: Text('Page 2'),
+                      ),
                     ),
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    child: const Center(
-                      child: Text('Page 3'),
+                    Container(
+                      color: Colors.orange,
+                      child: const Center(
+                        child: Text('Page 3'),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
           bottomNavigationBar: NavigationBar(
             destinations: _navigationButtonList,
