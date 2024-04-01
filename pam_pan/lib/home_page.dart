8f0a4d6e299ff31bf401b2d404b814297b406ea5
KhalilKK <khairkhalilkhaled@gmail.com>
2024-04-01 19:45:46 +0400

    Fixed home page

--- pam_pan/lib/home_page.dart
+++ pam_pan/lib/home_page.dart
@@ -7,30 +7,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ButtonContentsManager.buttonContents[7],
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.amber,
-                ),
-                child: const Center(
-                  child: Text(
-                      'Pam and buttons and the pantry - incomplete - is clickable and will take you to the full pantry page'),
-                ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ButtonContentsManager.buttonContents[7],
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.amber,
+              ),
+              child: const Center(
+                child: Text(
+                    'Pam and buttons and the pantry - incomplete - is clickable and will take you to the full pantry page'),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
