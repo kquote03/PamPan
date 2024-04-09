415c6ea45312b8ff3fc69a1aebd99b4eb43f26e5
s4rra <66dents@gmail.com>
2024-04-09 22:28:18 +0400

    ugh

--- pam_pan/lib/profilestuff/profile.dart
+++ pam_pan/lib/profilestuff/profile.dart
@@ -2,8 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:pam_pan/profilestuff/paymentmethod.dart';
 import 'editprofile.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  int points = 0;
+  int nextLevelThreshold = 100;
+  double progressValue = 0.0;
+  int level = 0;
+
+  void addPoints() {
+    setState(() {
+      points += 10;
+      if (points >= nextLevelThreshold) {
+        level++;
+        nextLevelThreshold = (level + 1) * 100;
+        progressValue = 0.0;
+      } else {
+       progressValue = points / nextLevelThreshold;
+      }
+    });
+  }
+
+  void subtractPoints() {
+    setState(() {
+      points -= 10;
+      if (points < 0) points = 0;
+      if (points < level * 100) {
+        level--;
+        if (level < 0) level = 0;
+        nextLevelThreshold = (level + 1) * 100;
+      }
+      progressValue = points / nextLevelThreshold;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,8 +64,7 @@ class ProfilePage extends StatelessWidget {
                     radius: 40,
                     backgroundColor: Colors.blueGrey[700],
                     child: const CircleAvatar(
-                      radius:
-                          35,
+                      radius: 35,
                       backgroundColor: Colors.transparent,
                       child: Icon(
                         Icons.person,
@@ -45,6 +80,33 @@ class ProfilePage extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 20,
                   ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            LinearProgressIndicator(
+              value: progressValue,
+              backgroundColor: Colors.grey[300],
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.black),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Level: $level',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const Expanded(child: SizedBox()),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    addPoints();
+                  },
+                  child: const Text('+10 points'),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: subtractPoints,
+                  child: const Text('-10 points'),
                 ),
               ],
             ),
