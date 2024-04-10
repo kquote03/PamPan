c9775dcf5491c3cfdcb94bb9ab8d0de645ea9816
KhalilKK <khairkhalilkhaled@gmail.com>
2024-04-10 18:43:16 +0400

    commit

--- pam_pan/lib/calendars(temp)/calendar_page.dart
+++ pam_pan/lib/calendars(temp)/calendar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pam_pan/data/buttons_contents_manager.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -15,34 +16,44 @@ class _CalendarPage extends State<CalendarPage> {
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      firstDay: DateTime(2020, 1, 1),
-      lastDay: DateTime(2222, 12, 31),
-      focusedDay: DateTime.now(),
-      calendarFormat: _calendarFormat,
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        if (!isSameDay(_selectedDay, selectedDay)) {
-          setState(() {
-            _selectedDay = selectedDay;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(ButtonContentsManager.pageLabels[2]),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: TableCalendar(
+          firstDay: DateTime(2020, 1, 1),
+          lastDay: DateTime(2222, 12, 31),
+          focusedDay: DateTime.now(),
+          calendarFormat: _calendarFormat,
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          },
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              // Call `setState()` when updating calendar format
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          onPageChanged: (focusedDay) {
+            // No need to call `setState()` here
             _focusedDay = focusedDay;
-          });
-        }
-      },
-      onFormatChanged: (format) {
-        if (_calendarFormat != format) {
-          // Call `setState()` when updating calendar format
-          setState(() {
-            _calendarFormat = format;
-          });
-        }
-      },
-      onPageChanged: (focusedDay) {
-        // No need to call `setState()` here
-        _focusedDay = focusedDay;
-      },
-    );
+          },
+        ));
   }
 }
--- pam_pan/lib/page_skeleton.dart
+++ pam_pan/lib/page_skeleton.dart
@@ -44,105 +44,114 @@ class _PageSkeleton extends State<PageSkeleton> {
   Widget build(context) {
     return MaterialApp(
       color: Colors.white,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(ButtonContentsManager.pageLabels[index]),
-          leading: IconButton(
-            icon: const Icon(Icons.help, size: 30, color: Colors.black),
-            onPressed: () {},
-          ),
-          actions: [
-            Text(
-              DateFormat.MMMM().format(DateTime.now()),
-              style: const TextStyle(fontSize: 20),
+      home: Builder(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: Text(ButtonContentsManager.pageLabels[index]),
+            leading: IconButton(
+              icon: const Icon(Icons.help, size: 30, color: Colors.black),
+              onPressed: () {},
             ),
-            IconButton(
-              icon: const Icon(Icons.calendar_month,
-                  size: 30, color: Colors.black),
-              onPressed: () {
-                index = 2;
-                // change index from 2-6 to access the diff calendar pages, look at ButtonContentsManager for more info
-                buttonClicked();
-              },
-            ),
-          ],
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        ),
-        body: currentPage,
-        floatingActionButton: FloatingActionButton.large(
-          onPressed: () {},
-          elevation: 0,
-          // isExtended: true,
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          shape: const CircleBorder(),
-          child: IconButton(
-            onPressed: () {},
-            icon: const Icon(Clarity.plus_circle_solid,
-                size: 55, color: Colors.black),
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(
-              icon: IconButton(
-                  onPressed: () {
-                    index = 0;
-                    buttonClicked();
-                  },
-                  icon: const Icon(Icons.home, size: 35, color: Colors.black)),
-              label: 'Home',
-            ),
-            NavigationDestination(
-                icon: IconButton(
-                  onPressed: () {
-                    index = 1;
-                    buttonClicked();
-                  },
-                  icon: const Icon(Icons.location_on,
-                      size: 35, color: Colors.black),
-                ),
-                label: 'Map'),
-            NavigationDestination(
-                icon: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.camera_alt,
-                      size: 1, color: Color.fromARGB(0, 0, 0, 0)),
-                ),
-                label: 'Map'),
-            NavigationDestination(
-                icon: IconButton(
-                  icon: const Icon(Icons.notifications,
-                      size: 35, color: Colors.black),
-                  onPressed: () {
-                    AwesomeNotifications().createNotification(
-                        content: NotificationContent(
-                      id: 1,
-                      channelKey: "basic_channel",
-                      title: "hello world",
-                      body: "yay noti",
-                    ));
-                  },
-                ),
-                label: 'Notifications'),
-            NavigationDestination(
-              icon: IconButton(
-                onPressed: () {
-                  index = 7;
-                  buttonClicked();
-                },
-                icon: const Icon(Icons.person, size: 35, color: Colors.black),
+            actions: [
+              Text(
+                DateFormat.MMMM().format(DateTime.now()),
+                style: const TextStyle(fontSize: 20),
               ),
-              label: 'Profile',
+              IconButton(
+                icon: const Icon(Icons.calendar_month,
+                    size: 30, color: Colors.black),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ButtonContentsManager.buttonContents[2];
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          ),
+          body: currentPage,
+          floatingActionButton: FloatingActionButton.large(
+            onPressed: () {},
+            elevation: 0,
+            // isExtended: true,
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+            shape: const CircleBorder(),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Clarity.plus_circle_solid,
+                  size: 55, color: Colors.black),
             ),
-          ],
-          backgroundColor: Colors.white,
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-          height: 70,
-          // onDestinationSelected: (value) {},
-          selectedIndex: 0,
-          surfaceTintColor: Colors.white,
-          indicatorColor: const Color.fromARGB(255, 255, 255, 255),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: NavigationBar(
+            destinations: [
+              NavigationDestination(
+                icon: IconButton(
+                    onPressed: () {
+                      index = 0;
+                      buttonClicked();
+                    },
+                    icon:
+                        const Icon(Icons.home, size: 35, color: Colors.black)),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                  icon: IconButton(
+                    onPressed: () {
+                      index = 1;
+                      buttonClicked();
+                    },
+                    icon: const Icon(Icons.location_on,
+                        size: 35, color: Colors.black),
+                  ),
+                  label: 'Map'),
+              NavigationDestination(
+                  icon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.camera_alt,
+                        size: 1, color: Color.fromARGB(0, 0, 0, 0)),
+                  ),
+                  label: 'Map'),
+              NavigationDestination(
+                  icon: IconButton(
+                    icon: const Icon(Icons.notifications,
+                        size: 35, color: Colors.black),
+                    onPressed: () {
+                      AwesomeNotifications().createNotification(
+                          content: NotificationContent(
+                        id: 1,
+                        channelKey: "basic_channel",
+                        title: "hello world",
+                        body: "yay noti",
+                      ));
+                    },
+                  ),
+                  label: 'Notifications'),
+              NavigationDestination(
+                icon: IconButton(
+                  onPressed: () {
+                    index = 7;
+                    buttonClicked();
+                  },
+                  icon: const Icon(Icons.person, size: 35, color: Colors.black),
+                ),
+                label: 'Profile',
+              ),
+            ],
+            backgroundColor: Colors.white,
+            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+            height: 70,
+            // onDestinationSelected: (value) {},
+            selectedIndex: 0,
+            surfaceTintColor: Colors.white,
+            indicatorColor: const Color.fromARGB(255, 255, 255, 255),
+          ),
         ),
       ),
     );
