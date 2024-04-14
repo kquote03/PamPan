84ebacfe4819b346293db5e811ba7296f15628cc
KhalilKK <khairkhalilkhaled@gmail.com>
2024-04-14 14:54:54 +0400

    size

--- pam_pan/lib/profile/profile_page.dart
+++ pam_pan/lib/profile/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pam_pan/profile/about_pam_page.dart';
@@ -45,239 +46,247 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 255, 250, 240),
-        title: Text(ButtonContentsManager.pageLabels[7]),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+    double screenHeight = MediaQuery.sizeOf(context).height;
+    return Builder(
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 255, 250, 240),
+          title: Text(ButtonContentsManager.pageLabels[7]),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
-      ),
-      backgroundColor: const Color.fromARGB(255, 255, 250, 240),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundColor: Colors.blueGrey[700],
-                  child: const CircleAvatar(
-                    radius: 35,
-                    backgroundColor: Colors.transparent,
-                    child: Icon(
-                      Icons.person,
-                      size: 30,
-                      color: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 255, 250, 240),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.blueGrey[700],
+                    child: const CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Colors.transparent,
+                      child: Icon(
+                        Icons.person,
+                        size: 30,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
+                  const SizedBox(width: 16),
+                  const Text(
+                    "'user'",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const EditProfile(),
+                    ),
+                  );
+                },
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      return Colors.transparent;
+                    },
+                  ),
                 ),
-                const SizedBox(width: 16),
-                const Text(
-                  "'user'",
+                child: const Text(
+                  'Edit',
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 0, 0, 0),
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const EditProfile(),
-                  ),
-                );
-              },
-              style: ButtonStyle(
-                overlayColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) {
-                    return Colors.transparent;
-                  },
-                ),
               ),
-              child: const Text(
-                'Edit',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
+              LinearProgressIndicator(
+                value: progressValue,
+                backgroundColor: Colors.grey[300],
+                valueColor: const AlwaysStoppedAnimation<Color>(Colors.black),
               ),
-            ),
-            LinearProgressIndicator(
-              value: progressValue,
-              backgroundColor: Colors.grey[300],
-              valueColor: const AlwaysStoppedAnimation<Color>(Colors.black),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Level: $level',
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    addPoints();
-                  },
-                  child: const Text('+10 points'),
-                ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: subtractPoints,
-                  child: const Text('-10 points'),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.45,
-            ),
-            SizedBox(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              const SizedBox(height: 8),
+              Text(
+                'Level: $level',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 16),
+              Row(
                 children: [
-                  Expanded(
-                    child: TextButton.icon(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const PaymentMethod(),
-                          ),
-                        );
-                      },
-                      icon: const Icon(
-                        Icons.payments_outlined,
-                        size: 20,
-                        color: Color.fromARGB(255, 34, 33, 33),
-                      ),
-                      label: const Align(
-                        alignment: Alignment(-1.0, 0.0),
-                        child: Text(
-                          "Payment Options",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            fontWeight: FontWeight.bold,
+                  ElevatedButton(
+                    onPressed: () {
+                      addPoints();
+                    },
+                    child: const Text('+10 points'),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: subtractPoints,
+                    child: const Text('-10 points'),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: screenHeight * 0.3,
+                ),
+              ),
+              SizedBox(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PaymentMethod(),
+                            ),
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.payments_outlined,
+                          size: 20,
+                          color: Color.fromARGB(255, 34, 33, 33),
+                        ),
+                        label: const Align(
+                          alignment: Alignment(-1.0, 0.0),
+                          child: Text(
+                            "Payment Options",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) {
-                            return const Color.fromARGB(255, 219, 219, 219);
-                          },
-                        ),
-                        shape: MaterialStateProperty.all<OutlinedBorder>(
-                          const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero,
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              return const Color.fromARGB(255, 219, 219, 219);
+                            },
+                          ),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: TextButton.icon(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const AboutPam(),
-                          ),
-                        );
-                      },
-                      icon: const Icon(
-                        Icons.info_outline,
-                        size: 20,
-                        color: Color.fromARGB(255, 34, 33, 33),
-                      ),
-                      label: const Align(
-                        alignment: Alignment(-1.0, 0.0),
-                        child: Text(
-                          "About PamPan",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+              const SizedBox(height: 5),
+              SizedBox(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const AboutPam(),
+                            ),
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.info_outline,
+                          size: 20,
+                          color: Color.fromARGB(255, 34, 33, 33),
+                        ),
+                        label: const Align(
+                          alignment: Alignment(-1.0, 0.0),
+                          child: Text(
+                            "About PamPan",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) {
-                            return const Color.fromARGB(255, 219, 219, 219);
-                          },
-                        ),
-                        shape: MaterialStateProperty.all<OutlinedBorder>(
-                          const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero,
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              return const Color.fromARGB(255, 219, 219, 219);
+                            },
+                          ),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: TextButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.exit_to_app,
-                        size: 20,
-                        color: Color.fromARGB(255, 34, 33, 33),
-                      ),
-                      label: const Align(
-                        alignment: Alignment(-1.0, 0.0),
-                        child: Text(
-                          "Log Out",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold,
+              const SizedBox(height: 5),
+              SizedBox(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.exit_to_app,
+                          size: 20,
+                          color: Color.fromARGB(255, 34, 33, 33),
+                        ),
+                        label: const Align(
+                          alignment: Alignment(-1.0, 0.0),
+                          child: Text(
+                            "Log Out",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) {
-                            return const Color.fromARGB(255, 219, 219, 219);
-                          },
-                        ),
-                        shape: MaterialStateProperty.all<OutlinedBorder>(
-                          const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero,
+                        style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                              return const Color.fromARGB(255, 219, 219, 219);
+                            },
+                          ),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
