e1cd7a4cb637892182941201b4efc35ec4497b5d
KhalilKK <khairkhalilkhaled@gmail.com>
2024-04-16 18:00:02 +0400

    somebody once told me

--- pam_pan/ios/Runner/AppDelegate.swift
+++ pam_pan/ios/Runner/AppDelegate.swift
@@ -4,6 +4,9 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
--- pam_pan/lib/main.dart
+++ pam_pan/lib/main.dart
@@ -1,29 +1,16 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:pam_pan/accountsetup/libdb.dart';
 import 'package:pam_pan/page_skeleton.dart';
+
+int id = 0;
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() async {
   // v-- might make the app take longer to load (needed to make sure the db can initialize)
   WidgetsFlutterBinding.ensureInitialized();
   final db = LibDB().initializeDB();
-  await AwesomeNotifications().initialize(null, [
-    NotificationChannel(
-      channelGroupKey: "basic_channel_group",
-      channelKey: "basic_channel",
-      channelName: "basic name",
-      channelDescription: "basic desc",
-    )
-  ], channelGroups: [
-    NotificationChannelGroup(
-      channelGroupKey: "basic_channel_group",
-      channelGroupName: "basic group",
-    )
-  ]);
-  // bool isAllowedToSendNotifications =
-  // await AwesomeNotifications().isNotificationAllowed();
-  // if (!isAllowedToSendNotifications) {
-  AwesomeNotifications().requestPermissionToSendNotifications();
-  // }
+
   runApp(const PageSkeleton());
 }
