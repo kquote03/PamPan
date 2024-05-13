804b26b760f551437d32978c11d7e1debe0ccc32
KhalilKK <khairkhalilkhaled@gmail.com>
2024-05-13 14:33:35 +0400

    notis test, doesn't affect anything

--- pam_pan/lib/main.dart
+++ pam_pan/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:pam_pan/accountsetup/front_page.dart';
 import 'package:pam_pan/accountsetup/libdb.dart';
 import 'package:pam_pan/notifications/local_notifications.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() async {
   // v-- might make the app take longer to load (needed to make sure the db can initialize)
@@ -19,3 +20,42 @@ void main() async {
     ),
   );
 }
+
+//ignore it is for tests VV
+
+// void main() {
+  // final List<List<String>> items = [
+    // ["2024-05-18", "Item4"],
+    // ["2024-05-15", "Item1"],
+    // ["2024-05-16", "Item2"],
+    // ["2024-05-17", "Item3"],
+    // ["2024-05-19", "Item5"],
+  // ];
+// 
+  // List<List<String>> sortList(List<List<String>> list) {
+    // list.sort((a, b) => a[0].compareTo(b[0]));
+    // return list;
+  // }
+// 
+  // int daysBetween(DateTime from, DateTime to) {
+    // from = DateTime(from.year, from.month, from.day);
+    // to = DateTime(to.year, to.month, to.day);
+    // return (to.difference(from).inHours / 24).round();
+  // }
+// 
+  // DateTime stringToDate(String date) {
+    // return DateTime.parse(date);
+  // }
+// 
+  // for (int i = 0; i < items.length; i++) {
+    // print(items[i]);
+  // }
+// 
+  // List<List<String>> newList = sortList(items);
+// 
+  // for (int i = 0; i < newList.length; i++) {
+    // print(newList[i]);
+  // }
+// 
+  // print(daysBetween(stringToDate(newList[1][0]), stringToDate(newList[0][0])));
+// }
---
+++ pam_pan/lib/notifications/expiry_test.dart
@@ -0,0 +1,24 @@
+class ExpiryTest {
+  static final List<List<String>> items = [
+    ["2024-05-15", "Item1"],
+    ["2024-05-16", "Item2"],
+    ["2024-05-17", "Item3"],
+    ["2024-05-18", "Item4"],
+    ["2024-05-19", "Item5"],
+  ];
+
+  List<List<String>> sortList(List<List<String>> list) {
+    list.sort((a, b) => a[0].compareTo(b[0]));
+    return list;
+  }
+
+  DateTime stringToDate(String date) {
+    return DateTime.parse(date);
+  }
+
+  int daysBetween(DateTime from, DateTime to) {
+    from = DateTime(from.year, from.month, from.day);
+    to = DateTime(to.year, to.month, to.day);
+    return (to.difference(from).inHours / 24).round();
+  }
+}
