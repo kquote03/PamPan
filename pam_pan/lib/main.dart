07998a0a4c449fa546e9ab1f11b49a9680f7b0f6
kquote03 <kquote03@gmail.com>
2024-05-14 12:26:07 +0400

    removed outdated comment

--- pam_pan/lib/main.dart
+++ pam_pan/lib/main.dart
@@ -7,7 +7,6 @@ import 'package:pam_pan/accountsetup/libdb.dart';
 import 'package:pam_pan/notifications/local_notifications.dart';
 
 void main() async {
-  // v-- might make the app take longer to load (needed to make sure the db can initialize)
   WidgetsFlutterBinding.ensureInitialized();
   await LocalNotifications.init();
 
