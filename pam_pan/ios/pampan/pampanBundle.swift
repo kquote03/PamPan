3d587f4489ec7ca2dd2aceef525cac57f3c06140
s4rra <66dents@gmail.com>
2024-05-20 10:37:29 +0400

    widget

---
+++ pam_pan/ios/pampan/pampanBundle.swift
@@ -0,0 +1,16 @@
+//
+//  pampanBundle.swift
+//  pampan
+//
+//  Created by Sarra on 19/05/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct pampanBundle: WidgetBundle {
+    var body: some Widget {
+        pampan()
+    }
+}
