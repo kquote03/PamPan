3cafc8c9de155787be3938e5505bb8cde323920b
kQuote03 <kquote03@gmail.com>
2024-05-24 11:44:58 +0400

    Remove OpenFoodFacts dart test

--- openfoodfactsexample/bin/openfoodfactsexample.dart
+++
@@ -1,6 +0,0 @@
-import 'package:http/http.dart' as http;
-
-void main(List<String> arguments) async {
-  print(await http.read(Uri.parse(
-      "https://world.openfoodfacts.org/api/v0/product/8690504019091.json")));
-}
