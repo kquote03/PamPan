27f37e4b2e1dac94e63b3548c3f708c253bff365
KhalilKK <khairkhalilkhaled@gmail.com>
2024-05-24 13:36:45 +0400

    firebase initial configs 2

--- pam_pan/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pam_pan/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import device_info_plus
 import file_selector_macos
-import firebase_app_check
 import firebase_core
 import flutter_local_notifications
 import flutter_web_auth_2
@@ -21,7 +20,6 @@ import window_to_front
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
