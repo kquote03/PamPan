166bad0d7e46b68afd6ef34242d4e6b1dfb72cbb
KhalilKK <khairkhalilkhaled@gmail.com>
2024-05-28 03:47:09 +0400

    multi modal methods created

--- pam_pan/lib/main.dart
+++ pam_pan/lib/main.dart
@@ -12,13 +12,10 @@ import 'package:pam_pan/welcome/welcome_name.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-<<<<<<< HEAD
 import 'package:appwrite/appwrite.dart';
 import 'package:pam_pan/backend/appwrite_client.dart';
-=======
 import 'package:firebase_vertexai/firebase_vertexai.dart';
 import 'package:firebase_core/firebase_core.dart';
->>>>>>> 30b8cf1 (initial stages)
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,7 +37,6 @@ void main() async {
             email: "email@example.com", password: "password123")
         .timeout(const Duration(seconds: 10));
 
-<<<<<<< HEAD
     var result = await account.get();
     print("Currently signed in as: " + (result?.email ?? "error"));
 
@@ -55,7 +51,6 @@ void main() async {
     if (!e.toString().contains("user_session_already_exists"))
       runApp(const MaterialApp(home: ConnectionError()));
   }
-=======
   final model = FirebaseVertexAI.instance
       .generativeModel(model: 'gemini-1.5-flash-preview-0514');
 
@@ -69,5 +64,4 @@ void main() async {
       home: WelcomeName(),
     ),
   );
->>>>>>> 30b8cf1 (initial stages)
 }
