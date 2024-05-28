c2267e3a896688e6f19661601fc97dd960cf31b5
kQuote03 <kquote03@gmail.com>
2024-05-28 22:54:18 +0400

    Fix a few merger issues

--- pam_pan/lib/map_page.dart
+++ pam_pan/lib/map_page.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:location/location.dart';
+import 'package:pam_pan/bottom_bar.dart';
 import 'package:pam_pan/consts.dart';
 import 'package:pam_pan/home_page.dart';
 import 'package:pam_pan/pantry/add_item_page.dart';
-import 'package:pam_pan/records.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class MapPage extends StatefulWidget {
@@ -120,82 +120,7 @@ class _MapPageState extends State<MapPage> {
               },
               polylines: Set<Polyline>.of(polylines.values),
             ),
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: const Color.fromARGB(255, 255, 250, 240),
-        destinations: [
-          NavigationDestination(
-            icon: IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return const HomePage();
-                    },
-                  ),
-                );
-              },
-              icon: const Icon(
-                Icons.home,
-                size: 35,
-                color: Colors.black,
-              ),
-            ),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: IconButton(
-              onPressed: () {},
-              icon:
-                  const Icon(Icons.location_on, size: 35, color: Colors.black),
-            ),
-            label: 'Map',
-          ),
-          NavigationDestination(
-            icon: IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return const AddItemPage();
-                    },
-                  ),
-                );
-              },
-              icon: const Icon(Clarity.plus_circle_solid,
-                  size: 35, color: Colors.black),
-            ),
-            label: 'Camera',
-          ),
-          NavigationDestination(
-            icon: IconButton(
-              icon: const Icon(
-                Icons.receipt,
-                size: 35,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return const Records();
-                    },
-                  ),
-                );
-              },
-            ),
-            label: 'Records',
-          ),
-        ],
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-        height: 70,
-        // onDestinationSelected: (value) {},
-        selectedIndex: 0,
-        surfaceTintColor: const Color.fromARGB(255, 255, 255, 242),
-        indicatorColor: const Color.fromARGB(255, 255, 255, 242),
-      ),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
 
--- pam_pan/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pam_pan/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,8 @@ import file_selector_macos
 import firebase_app_check
 import firebase_core
 import flutter_local_notifications
-import location
 import flutter_web_auth_2
+import location
 import package_info_plus
 import path_provider_foundation
 import sqflite
@@ -24,8 +24,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
