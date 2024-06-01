90e5ed9f7145f94a860eb42c2edd4cad3be24ae1
miriamgeorge <miriamgeorge473@gmail.com>
2024-06-01 09:25:02 +0400

    ui

--- pam_pan/lib/login and signup/login.dart
+++ pam_pan/lib/login and signup/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pam_pan/backend/appwrite_client.dart';
 import 'package:pam_pan/home_page.dart';
 
@@ -23,9 +24,7 @@ class _LoginPageState extends State<LoginPage> {
 
   Future<void> _login() async {
     if (_formKey.currentState!.validate()) {
-      // Perform login action
-      print('Username: ${_usernameController.text}');
-      print('Password: ${_passwordController.text}');
+      _showProcessingDialog(context, "Processing", "Logging in...");
 
       try {
         await account
@@ -35,15 +34,21 @@ class _LoginPageState extends State<LoginPage> {
             .timeout(const Duration(seconds: 10));
 
         var result = await account.get();
-        print("Currently signed in as: " + (result?.email ?? "error"));
+        print("Currently signed in as: ${result?.email ?? "error"}");
 
-        Navigator.pop(context);
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
+        // Close the processing dialog after 2 seconds
+        await Future.delayed(const Duration(seconds: 1));
+        Navigator.pop(context); // Close the processing dialog
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
           return HomePage();
         }));
       } catch (e) {
+        await Future.delayed(const Duration(seconds: 1));
+        Navigator.pop(context); // Close the processing dialog
         if (e.toString().contains("user_invalid_credentials")) {
-          _showInvalidCredentialDialog(context);
+          _showModalDialog(context, "ERROR",
+              "Email or password is invalid, please try again.", "Close");
         }
       }
     }
@@ -56,79 +61,238 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text('Login'),
+        automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                controller: _usernameController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: _login,
-                  child: const Text('Login'),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Image.asset(
+                //   'assets/logo/pampan_logo.png',
+                //   height: 100,
+                //   alignment: Alignment.center,
+                // ),
+                const SizedBox(height: 20),
+                Text('Email', style: GoogleFonts.mukta(fontSize: 17)),
+                const SizedBox(height: 3),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    if (!RegExp(r'^[a-zA-Z0-9._%+-]+@example\.com$')
+                        .hasMatch(value)) {
+                      return 'Please enter a valid email with @example.com';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-            ],
+                const SizedBox(height: 15),
+                Text('Password', style: GoogleFonts.mukta(fontSize: 17)),
+                const SizedBox(height: 3),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _login,
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStateProperty.all<Color>(Colors.black),
+                      overlayColor: WidgetStateProperty.resolveWith<Color>(
+                        (Set<WidgetState> states) {
+                          return Colors.white;
+                        },
+                      ),
+                      shape: WidgetStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.5),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Login',
+                      style: GoogleFonts.mukta(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          // Placeholder for future sign up functionality
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-}
 
-_showInvalidCredentialDialog(context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext builderContext) {
-      return Dialog(
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-        child: Container(
-          constraints: const BoxConstraints(maxHeight: 150),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                const Center(
-                  child: Text(
-                    "Email or password is invalid, please try again.",
-                    style: TextStyle(fontSize: 24),
+  void _showProcessingDialog(
+      BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext builderContext) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: EdgeInsets.zero,
+          content: Container(
+            constraints: const BoxConstraints(maxHeight: 200),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context, rootNavigator: true).pop();
-                    },
-                    child: const Text("Close"))
-              ],
+                  const SizedBox(height: 20),
+                  Center(
+                    // Center the message text
+                    child: Text(
+                      message,
+                      style: const TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    },
-  );
+        );
+      },
+    );
+  }
+
+  void _showModalDialog(
+      BuildContext context, String title, String message, String buttonText) {
+    showDialog(
+      context: context,
+      builder: (BuildContext builderContext) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: EdgeInsets.zero,
+          content: Container(
+            constraints: const BoxConstraints(maxHeight: 200),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    // Center the message text
+                    child: Text(
+                      message,
+                      style: const TextStyle(fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  if (buttonText.isNotEmpty)
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                WidgetStateProperty.all<Color>(Colors.black),
+                            overlayColor:
+                                WidgetStateProperty.resolveWith<Color>(
+                              (Set<WidgetState> states) {
+                                return const Color.fromARGB(255, 219, 219, 219);
+                              },
+                            ),
+                            shape: WidgetStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context, rootNavigator: true).pop();
+                          },
+                          child: Text(
+                            buttonText,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
